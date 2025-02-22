c8f240efbcee1b679ffb87b53d70473f87c92fbb
zahit18 <zahithdez@gmail.com>
2025-02-21 19:47:33 -0800

    Antes del SEED

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -15,7 +16,9 @@ import { CommonModule } from './common/common.module';
 
     PokemonModule,
 
-    CommonModule
+    CommonModule,
+
+    SeedModule
   ], 
   controllers: [],
   providers: [],
